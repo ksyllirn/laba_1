549fcbde6e78a6d99c5eb9b9df757811a38d3466
Ксения Куликовских <115486017+ksyllirn@users.noreply.github.com>
2024-12-23 23:39:50 +0500

    Update laba.cs

--- laba/laba.cs
+++ laba/laba.cs
@@ -18,8 +18,17 @@ namespace SimpleFormApp
         private TextBox inputTextBox4_2;
         private TextBox outputTextBox_2;
 
+        private Label preinputLabel_3;
+        private TextBox inputTextBox_3;
+        private TextBox outputTextBox_3;
+        private ListBox outputListBox_3;
         public MainForm()
         {
+            Text = "Задание 1-3";
+            Width = 1000;
+            Height = 1000;
+            StartPosition = FormStartPosition.CenterScreen;
+
             preinputLabel_1 = new Label()
             {
                 Top = 20,
@@ -125,15 +134,52 @@ namespace SimpleFormApp
             preinputLabel_2.Text = "Цена A, B, C и введённую сумма D";
             outputTextBox_2.Text = "Сдача: ";
 
-            Text = "Задание 1-2";
-            Width = 1000;
-            Height = 1000;
-            StartPosition = FormStartPosition.CenterScreen;
-
             inputTextBox1_2.TextChanged += InputTextBox_TextChanged_2;
             inputTextBox2_2.TextChanged += InputTextBox_TextChanged_2;
             inputTextBox3_2.TextChanged += InputTextBox_TextChanged_2;
             inputTextBox4_2.TextChanged += InputTextBox_TextChanged_2;
+
+            preinputLabel_3 = new Label()
+            {
+                Top = 280,
+                Left = 20,
+                Width = 200,
+            };
+
+
+            inputTextBox_3 = new TextBox()
+            {
+                Top = 320,
+                Left = 20,
+                Width = 200
+            };
+
+            outputTextBox_3 = new TextBox()
+            {
+                Top = 360,
+                Left = 20,
+                Width = 200,
+                ReadOnly = true
+            };
+
+            outputListBox_3 = new ListBox()
+            {
+                Top = 280,
+                Left = 250,
+                Width = 200,
+                Height = 80
+            };
+
+            Controls.Add(preinputLabel_3);
+            Controls.Add(inputTextBox_3);
+            Controls.Add(outputTextBox_3);
+            Controls.Add(outputListBox_3);
+
+            preinputLabel_3.Text = "Введите число";
+            outputTextBox_3.Text = "";
+
+            inputTextBox_3.TextChanged += InputTextBox_TextChanged_3;
+
         }
 
         private void InputTextBox_TextChanged_1(object sender, EventArgs e)
@@ -186,11 +232,48 @@ namespace SimpleFormApp
 
         private string ProcessOutput_2(float value1, float value2, float value3, float total_parsed)
         {
-            return $"Сдача: {total_parsed - (value1+value2+value3)}";
+            float result_2 = total_parsed - (value1 + value2 + value3);
+            if (result_2 >= 0)
+            {
+                return $"Сдача: {result_2}";
+            }
+            else
+            {
+                return $"Недостаточно средств";
+            }
         }
 
+        private void InputTextBox_TextChanged_3(object sender, EventArgs e)
+        {
+            string input = inputTextBox_3.Text;
+            if (int.TryParse(input, out int value))
+            {
+                outputTextBox_3.Text = ProcessOutput_3(value);
+            }
+            else
+            {
+                outputTextBox_3.Text = "Неверный формат ввода";
+            }
+        }
 
+        private string ProcessOutput_3(int input)
+        {
+            var numbers = new Dictionary<int, string>()
+            {
+                [0] = "Zero",[1] = "One",[2] = "Two",[3] = "Three",[4] = "Four",[5] = "Five",[6] = "Six",[7] = "Seven",[8] = "Eight",[9] = "Nine"
+            };
 
+            string num = "";
+            if (numbers.TryGetValue(input, out num))
+            {
+                outputListBox_3.Items.Add(num);
+                return $"Добавленное в список число: {num}";
+            }
+            else
+            {
+                return $"Неверный формат ввода";
+            }
+        }
 
         [STAThread]
         public static void Main()
